cc61acfb62d39ed97fc0d1781331c040e2a8f9ad
accors <61780738+accors@users.noreply.github.com>
2022-10-05 15:38:29 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,12 +3,15 @@ set -e
 cloneRepo() {
 if [ ! -d "/app/Sub-Store/.git" ]; then
 echo "未检查到Sub-Store仓库，拉取中..."
-git clone https://github.com/sub-store-org/Sub-Store /app
-git clone https://github.com/sub-store-org/Sub-Store-Front-End /app 
+cd /app
+git clone https://github.com/sub-store-org/Sub-Store
+git clone https://github.com/sub-store-org/Sub-Store-Front-End
+return 0 
 else
 echo "更新Sub-Store仓库..."
-git -C /app/Sub-Store fetch --all
-git -C /app/Sub-Store-Front-End fetch --all
+cd /app/Sub-Store && git pull
+cd /app/Sub-Store-Front-End && git pull
+return 0
 fi
 }
 cloneRepo
@@ -16,6 +19,5 @@ echo "仓库拉取成功，安装依赖中..."
 cd Sub-Store-Front-End && pnpm install
 cd .. && cd Sub-Store/backend && pnpm install
 echo "依赖安装完成，启动后端与面板..."
-cd && cd Sub-Store-Front-End && pnpm dev
-pm2-runtime start sub-store.min.js
-
+pm2-runtime start /app/Sub-Store/backend/sub-store.min.js
+cd /app && cd Sub-Store-Front-End && pnpm dev
