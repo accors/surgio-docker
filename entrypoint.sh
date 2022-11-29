d00faab6085f0f195747f5b816def4e0a22ef9f7
accors <61780738+accors@users.noreply.github.com>
2022-11-29 22:54:51 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,9 +16,8 @@ fi
 }
 cloneRepo
 echo "仓库拉取成功，安装依赖中..."
-cd Sub-Store-Front-End && pnpm install
-cd .. && cd Sub-Store/backend && pnpm install
+cd Sub-Store/backend && pnpm i
+cd .. && cd Sub-Store-Front-End && sed -i 's/192.168.1.19/127.0.0.1/g' .env.development && pnpm i
 echo "依赖安装完成，启动后端与面板..."
-pm2 start /app/Sub-Store/backend/sub-store.min.js
-cd /app/Sub-Store-Front-End 
-sed -i 's/192.168.1.19/127.0.0.1/g' .env.development && pnpm dev
+cd /app/Sub-Store && pm2 start "npm run serve" --name sub-store-backend
+cd /app/Sub-Store-Front-End && pm2 start "npm run dev" --name sub-store-frontend
